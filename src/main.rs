671a85f00bfa1fb219fb6d630e80e139c5bfa57e
Markus Kolb <github@tower-net.de>
2022-01-11 00:59:10 +0100

    provide data for sizes 4098..8429

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,9 @@ lazy_static! {
         for &size in &[4096_usize, 4097, 8429, 8430, 10000, 40960, 409600, 4096000] {
             m.insert(size, " ".repeat(size));
         }
+        for size in 4098..8429 {
+            m.insert(size, " ".repeat(size));
+        }
         m
     };
 }
